604be2afbde7b25426e2e16dbd7b76a56e945cb3
Yashmalbhage01 <87193552+Yashmalbhage01@users.noreply.github.com>
2024-05-10 16:31:05 +0530

    userProfile display using tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UserProfile from './userProfile';
+function App() {
+  return (
+    <div className="App">
+      <UserProfile/>
+      
+    </div>
+  );
+}
+
+export default App;
